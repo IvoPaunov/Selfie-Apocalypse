5e2f42f2e0d1f9fa94363e5ca4b1a2209ac04bfc
Ivo Paounov <ivo.paunov@gmail.com>
2016-02-02 18:56:35 +0200

    added LIvesStatusBar custom view
    Just shows 3 hearts yet.
    Added some images for static elements

---
+++ Selfie apocalypse/Selfie apocalypse/LivesStatusBar.swift
@@ -0,0 +1,47 @@
+//
+//  LivesStatusBar.swift
+//  Selfie apocalypse
+//
+//  Created by Ivko on 2/2/16.
+//  Copyright © 2016 Ivo Paounov. All rights reserved.
+//
+
+import UIKit
+
+class LivesStatusBar: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    
+    var view:UIView!;
+
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        loadViewFromNib ()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        loadViewFromNib ()
+    }
+    func loadViewFromNib() {
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let nib = UINib(nibName: "LivesStatusBar", bundle: bundle)
+        let view = nib.instantiateWithOwner(self, options: nil)[0] as! UIView
+        view.frame = bounds
+        view.sizeToFit()	
+        view.setNeedsUpdateConstraints()
+        	self.addSubview(view);
+        
+        
+        
+    }
+
+}
